bd4dedd728c684db3a61aeb74732eb6d32bb942e
Austin Roberts <manuela.garcia.dev@gmail.com>
2018-04-11 16:05:43 -0700

    This will move from old repo

---
+++ js/austinscript.js
@@ -0,0 +1,22 @@
+// $(document).ready(function(){
+//   alert("hi")
+//   match();
+// });
+
+var dummyVar1="A";
+var dummyVar2="A";
+
+function match(dummyVar1, dummyVar2){
+  if (dummyVar1 === dummyVar2){
+    return true
+  } else {
+    console.log("else")
+    // flip2Default();
+    return false
+    }
+};
+
+// function flip2Default(){
+//   $("#dummyCard1Id").hide();
+//   $("#dummyCard2Id").hide();
+// }
